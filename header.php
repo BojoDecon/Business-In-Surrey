ce7be6616cacf9232c4eda60907c04a1c134df02
Mutte Chen <mutte109@gmail.com>
2021-11-23 20:09:40 -0800

    Created temp header for all pages

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<main>
+    <a href="index.php" class="centered">All Recipes</a>
+    <a href="add-recipe.php" class="centered">Add a new recipe</a>
+    <hr>
