cbfcd9badb3de1279bc3c0e44f6a7df62dcec6d7
Mutte Chen <mutte109@gmail.com>
2021-11-24 15:09:11 -0800

    WIP for index

--- index.php
+++ index.php
@@ -3,8 +3,22 @@ require('header.php');
 ?>
 
 <main>
-	<div class="container">
-		
+	<div class="banner">
+		<div class="container">
+			<div class="col-2"></div>
+			<div class="col-2">
+				<img src="img/logo.png" width="10">
+			</div>
+			<div class="col-6">
+				<h1>Surrey's business depository of all things local.</h1>
+				<h2>Support local and <span>Discover.</span></h2>
+
+			</div>
+			<div class="col-2"></div>
+		</div>
+	</div>
+
+	<div class="search-bar">
 	</div>
 </main>
 
