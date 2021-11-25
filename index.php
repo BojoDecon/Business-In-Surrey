5ff174c9e495e5d2856b66ba21aa8cb9e35bf78d
Mutte Chen <mutte109@gmail.com>
2021-11-24 16:58:42 -0800

    WIP php for index

--- index.php
+++ index.php
@@ -19,6 +19,23 @@ require('header.php');
 	</div>
 
 	<div class="search-bar">
+		<div class="container">
+			<div class="col-2"></div>
+			<div class="col-8">
+				<form action="index.php" method="post" class="container">
+					<input type="button" name="search_type" value="Service" autofocus>
+					<input type="button" name="search_type" value="Nearby">
+					<div class="col-10"></div>
+					<input type="text" name="search_input" class="col-10" placeholder="<?php if($_POST["search_type"] == "Service") { echo "Input keywords. i.e. \"pizza\""; }?>">
+					<input type="submit" name="search_btn" value="Discover" class="col-2">
+				</form>
+			</div>
+			<div class="col-2"></div>
+		</div>
+	</div>
+
+	<div class="search_results">
+		
 	</div>
 </main>
 
