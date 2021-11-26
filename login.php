7b6f67a3b766973470aa09b9e72c674cff7775bb
Christian Lim <cl100@live.ca>
2021-11-25 19:25:13 -0800

    Login Page
    Contains content

--- login.php
+++ login.php
@@ -5,12 +5,22 @@ require('header.php');
 <main>
   <div class="login-page">
     <div class="container">
-      <div class="col-2"></div>
-      <div class="col-8">
-        <p>test</p>
+      <div class="col-6">
+        <form class="container container" action="login.php" method="post">
+          <h3>Username:</h3>
+          <input type="text" name="username_input" id="username_input" class="col-12" placholder="">
+          <h3>Password:</h3>
+          <input type="text" name="password_input" id="password_input" class="col-12" placholder="">
+          <input type="submit" name="login_btn" value="Submit" class="col-12">
+        </form>
       </div>
-      <div class="col-2"></div>
-    </div>
+      <div class="col-1"></div>
+      <div class="col-5">
+        <div class="col-12">
+          <h1>Not registered?</h1>
+          <h2>Create an account <span><a href="register.php">Sign up.</a></span></h2>
+        </div>
+      </div>
   </div>
 </main>
 
